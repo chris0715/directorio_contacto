74d4d4d074b4894b8ee18fc97ea70488283dfaa9
Chris <christopher.encarnacion@gmail.com>
2018-04-02 22:50:43 -0400

    addend new files for data connection

--- config.js
+++ config.js
@@ -1,6 +1,14 @@
 module.exports = {
     dbconfig: {
-
+      database: process.env.DB_NAME || 'your_DBNAME',
+      username: process.env.DB_USER || 'YOUR_USER',
+      password: process.env.DB_PASS || "YOUR_PASS!",
+      dialect: 'mysql',
+      pool: {
+        max: 10,
+        min: 0,
+        idle: 10000
+      }
     },
     appPort: process.env.PORT || 3000
 }
--- server.js
+++ server.js
@@ -1,3 +1,15 @@
 const express = require('express')
 const app = express()
 const config = require('./config')
+const sequelizeIns = require('./sequelizeIns')(config.dbconfig)
+
+app.get('/api/list', (req, res) => {
+  
+})
+
+app.listen(config.appPort, _ => {
+  console.log('Application runnning on port ', config.appPort)
+})
+
+sequelizeIns.authenticate()
+.then( _ => console.log('It was connected'))
